2b3c8b9e7ee3abf55f102cd8d57fa4402d7cb68d
Kvetuse Husakova <huskvenimrah@gmail.com>
2021-06-21 08:03:07 +0200

    SideBlockDONE

--- lang/en/block_sideblock.php
+++ lang/en/block_sideblock.php
@@ -1,8 +1,43 @@
 <?php
-$string['pluginname'] = 'Simple HTML side block';
-$string['simplehtml'] = 'Simple HTML side block';
-$string['simplehtml:addinstance'] = 'Add a new simple HTML side block';
-$string['simplehtml:myaddinstance'] = 'Add a new simple side HTML block to the My Moodle page';
-$string['sideblock'] = 'Side Block';
-$string['done'] = "Finished: {$a}";
-$string['undone'] = "Unfinished";
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_sideblock', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_sideblock
+ * @copyright 2021 Husakova Kvetuse
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+    $string['author'] = 'Author: ';
+    $string['authorname'] = 'Květa';
+    $string['descallowhtml'] = 'descallowhtml';
+    $string['descconfig'] = 'descconfig';
+    $string['done'] = 'finished: {$a}';
+    $string['headerconfig'] = 'headerconfig';
+    $string['infocourse'] = 'Course ';
+    $string['labelallowhtml'] = 'labelallowhtml';
+    $string['pluginname'] = 'Side Block *.*';
+    $string['sideblock'] = 'Side Block';
+    $string['simplehtml'] = 'Simple HTML Side Block';
+    $string['simplehtml:addinstance'] = 'Add a new simple HTML Side Block';
+    $string['simplehtml:myaddinstance'] = 'Add a new Side Block HTML block to the My Moodle page';
+    $string['undone'] = 'unfinished';
+
+
+
+
+
